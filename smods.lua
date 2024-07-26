b4d948ba0322f4e4448e72fd801a78c5793fe71d
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2024-07-25 22:16:26 -0600

    feat: basic smods compat

---
+++ smods.lua
@@ -0,0 +1,15 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: DebugPlus
+--- MOD_ID: DebugPlus
+--- MOD_AUTHOR: [WilsontheWolf]
+--- MOD_DESCRIPTION: Better Debug Tools for Balatro 
+--- PREFIX: DEBUGPLUS
+
+
+-- Steamodded is not necessary. This just adds a bit of compatibility.
+SMODS.Atlas({
+    key = "modicon",
+    path = "modicon.png",
+    px = 32,
+    py = 32
+}):register()
