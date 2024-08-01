9ba027f9797e9497903980c40cab35214f7cf144
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2024-07-31 22:44:55 -0600

    feat: basic config

--- smods.lua
+++ smods.lua
@@ -13,3 +13,59 @@ SMODS.Atlas({
     px = 32,
     py = 32
 }):register()
+
+local config = {
+    logLevel = "INFO",
+}
+test = {config = config}
+
+function G.FUNCS.DP_config_callback(args)
+    print(require('debugplus-util').stringifyTable(args))
+end
+
+local function debugToggle(args)
+    _RELEASE_MODE = not args -- TODO: Save pref
+end
+
+
+local testValues = {
+    debugMode = not _RELEASE_MODE
+}
+
+SMODS.current_mod.config_tab = function()
+    return {
+        -- ROOT NODE
+        n = G.UIT.ROOT,
+        config = {r = 0.1, minw = 7, minh = 5, align = "tm", padding = 1, colour = G.C.BLACK},
+        nodes = {
+            {
+                -- COLUMN NODE TO ALIGN EVERYTHING INSIDE VERTICALLY
+                n = G.UIT.C,
+                config = {align = "tm", padding = 0.1, colour = G.C.BLACK},
+                nodes = {
+                        create_toggle({
+                            label = "Debug Mode",
+                            ref_table = testValues,
+                            ref_value = "debugMode",
+                            callback = debugToggle,
+                            info = {"Toggles everything in DebugPlus except the console"}
+                        }),
+                        create_option_cycle({
+                            options = {
+                                "DEBUG",
+                                "INFO",
+                                "WARNING",
+                                "ERROR",
+                            },
+                            current_option = 2, -- TODO: how to dynamically get me
+                            ref_table = test.config,
+                            ref_value = "logLevel",
+                            opt_callback = "DP_config_callback",
+                            info = {"Log Level"}
+                        }),
+                        
+                }
+            }
+        }
+    }
+end
