fbf0e9671a7ba9b54fe68fdec911922af9b2ac34
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2024-12-28 22:15:16 -0500

    misc: fix makerelease.sh

--- makerelease.sh
+++ makerelease.sh
@@ -10,6 +10,6 @@ fi
 rm DebugPlus.zip 2>/dev/null
 
 sed -i 's/"version": ".*"/"version": "'"$VERSION"'"/' smods.json
-zip -r ./DebugPlus.zip lovely/ assets/ *.lua README.MD *.txt docs/
+zip -r ./DebugPlus.zip lovely/ assets/ *.lua README.MD *.txt docs/ smods.json
 
 echo "Zip made for v$VERSION!"
